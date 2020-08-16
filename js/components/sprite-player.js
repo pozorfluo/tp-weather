b808637941d96211af74d6b7dd68cda3ef0b42bc
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-08-17 01:48:51 +0200

    Add example for sprite-player web component

--- js/components/sprite-player.js
+++ js/components/sprite-player.js
@@ -1,21 +1,39 @@
 "use strict";
 class SpritePlayer extends HTMLElement {
     constructor() {
-        var _a;
+        var _a, _b;
         super();
         this._running = true;
+        this._loop = null;
         this.attachShadow({ mode: 'open' });
         (_a = this.shadowRoot) === null || _a === void 0 ? void 0 : _a.appendChild(SpritePlayer._template.cloneNode(true));
+        this._sprite = (_b = this.shadowRoot) === null || _b === void 0 ? void 0 : _b.getElementById('sprite');
         const rules = this.shadowRoot.styleSheets[0].cssRules[0];
         this.css = rules.style;
     }
     static get observedAttributes() {
-        return ['width', 'height', 'anim', 'duration', 'frames'];
+        return ['width', 'height', 'anim', 'duration', 'frames', 'loop'];
     }
     attributeChangedCallback(name, oldValue, newValue) {
         this.css.setProperty('--' + name, (newValue !== null && newValue !== void 0 ? newValue : 0) + '');
     }
+    setLoopDuration(duration) {
+        this.css.setProperty('--duration', duration + 'ms');
+        return this;
+    }
+    setLoop(iterations) {
+        this.css.setProperty('animation-iteration-count', iterations);
+        return this;
+    }
+    setAnim(anim) {
+        this.css.setProperty('--anim', anim + '');
+        return this;
+    }
     play() {
+        if (this._loop) {
+            this.css.setProperty('animation-iteration-count', this._loop);
+            this._loop = null;
+        }
         this.css.setProperty('animation-play-state', 'running');
         this._running = true;
     }
@@ -23,28 +41,45 @@ class SpritePlayer extends HTMLElement {
         this.css.setProperty('animation-play-state', 'paused');
         this._running = false;
     }
-    stop() {
-        this.css.setProperty('animation-play-state', 'paused');
-        this._running = false;
-    }
     toggle() {
+        if (this._loop) {
+            this.css.setProperty('animation-iteration-count', this._loop);
+            this._loop = null;
+        }
         const toggle = this._running ? 'paused' : 'running';
         this.css.setProperty('animation-play-state', toggle);
         this._running = !this._running;
     }
+    toggleReset() {
+        if (this._running) {
+            this.css.setProperty('animation-name', 'none');
+        }
+        else {
+            if (this._loop) {
+                this.css.setProperty('animation-iteration-count', this._loop);
+                this._loop = null;
+            }
+            this.css.setProperty('animation-name', 'play');
+        }
+        this._running = !this._running;
+        
+    }
     pauseAfter() {
+        this._loop = this.css.getPropertyValue('--loop');
         this.css.setProperty('animation-iteration-count', '1');
+        this._running = false;
     }
 }
 SpritePlayer._template = (() => {
     const t = document.createElement('template');
     t.innerHTML = `\
       <style>
-      .sprite {
+      #sprite {
         --width : 64px;
         --height : 64px;
         --frames: 12;
         --anim : 0;
+        --loop : infinite;
         --y : calc(var(--height) * var(--anim) * -1);
         --x : calc(var(--width) * var(--frames) * -1);
         --duration: 333ms;
@@ -52,14 +87,14 @@ SpritePlayer._template = (() => {
         height: var(--height);
         background-origin: border-box;
         background: url('sprites/link.png') 0px var(--y);
-        animation: play var(--duration) steps(var(--frames)) infinite;
+        animation: play var(--duration) steps(var(--frames)) var(--loop);
       }
 
       @keyframes play {
         100% { background-position: var(--x) var(--y); }
       }
       </style>
-      <div class="sprite"></div>
+      <div id="sprite"></div>
       `;
     return t.content;
 })();
--- js/src/components/sprite-player.ts
+++ js/src/components/sprite-player.ts
@@ -1,14 +1,14 @@
 class SpritePlayer extends HTMLElement {
-
   static _template: DocumentFragment = (() => {
     const t = document.createElement('template');
-    t.innerHTML=`\
+    t.innerHTML = `\
       <style>
-      .sprite {
+      #sprite {
         --width : 64px;
         --height : 64px;
         --frames: 12;
         --anim : 0;
+        --loop : infinite;
         --y : calc(var(--height) * var(--anim) * -1);
         --x : calc(var(--width) * var(--frames) * -1);
         --duration: 333ms;
@@ -16,70 +16,119 @@ class SpritePlayer extends HTMLElement {
         height: var(--height);
         background-origin: border-box;
         background: url('sprites/link.png') 0px var(--y);
-        animation: play var(--duration) steps(var(--frames)) infinite;
+        animation: play var(--duration) steps(var(--frames)) var(--loop);
       }
 
       @keyframes play {
         100% { background-position: var(--x) var(--y); }
       }
       </style>
-      <div class="sprite"></div>
+      <div id="sprite"></div>
       `;
-      return t.content;
+    return t.content;
   })();
 
+  _sprite: HTMLElement;
   _running = true;
+  _loop: string | null = null;
 
-  css : CSSStyleDeclaration;
+  css: CSSStyleDeclaration;
 
   static get observedAttributes() {
-    return ['width', 'height', 'anim', 'duration', 'frames'];
+    return ['width', 'height', 'anim', 'duration', 'frames', 'loop'];
   }
 
-  constructor(){
+  constructor() {
     super();
     // sets and returns 'this.shadowRoot'
-    this.attachShadow({mode: 'open'});
+    this.attachShadow({ mode: 'open' });
     this.shadowRoot?.appendChild(SpritePlayer._template.cloneNode(true));
+    this._sprite = <HTMLElement>this.shadowRoot?.getElementById('sprite');
 
     const rules = (<ShadowRoot>this.shadowRoot).styleSheets[0].cssRules[0];
     /** @todo Fix the ugly linter quieting */
     this.css = (<any>rules).style;
   }
-
+  // _listener(event : Event){
+  //   console.log(event.type);
+  // }
+  // connectedCallback(){
+  //   console.log(this._sprite);
+  //   this._sprite.addEventListener("animationstart", this._listener, false);
+  //   this._sprite.addEventListener("animationend", this._listener, false);
+  //   this._sprite.addEventListener("animationiteration", this._listener, false);
+  // }
   attributeChangedCallback(
     name: string,
     oldValue: number | string | null,
     newValue: number | string | null
-  ) :void {
-    this.css.setProperty('--'+name, (newValue ?? 0) + '');
+  ): void {
+    this.css.setProperty('--' + name, (newValue ?? 0) + '');
   }
 
-  play():void {
+  setLoopDuration(duration : number) : this{
+    this.css.setProperty('--duration', duration + 'ms');
+    return this;
+  }
+
+  setLoop(iterations: string): this {
+    this.css.setProperty('animation-iteration-count', iterations);
+    return this;
+  }
+
+  setAnim(anim: number): this {
+    this.css.setProperty('--anim', anim + '');
+    return this;
+  }
+  play(): void {
+    /** Restore loop settings if any. */
+    if (this._loop) {
+      this.css.setProperty('animation-iteration-count', this._loop);
+      this._loop = null;
+    }
     this.css.setProperty('animation-play-state', 'running');
     this._running = true;
   }
-  
-  pause():void {
+
+  pause(): void {
     this.css.setProperty('animation-play-state', 'paused');
     this._running = false;
   }
 
-  stop():void {
-    this.css.setProperty('animation-play-state', 'paused');
-    this._running = false;
-  }
-
-  toggle():void {
+  toggle(): void {
+    /** Restore loop settings if any. */
+    if (this._loop) {
+      this.css.setProperty('animation-iteration-count', this._loop);
+      this._loop = null;
+    }
     const toggle = this._running ? 'paused' : 'running';
     this.css.setProperty('animation-play-state', toggle);
     this._running = !this._running;
   }
 
-  pauseAfter():void {
-    this.css.setProperty('animation-iteration-count', '1');
+  toggleReset(): void {
+    if (this._running) {
+      this.css.setProperty('animation-name', 'none');
+    } else {
+      /** Restore loop settings if any. */
+      if (this._loop) {
+        this.css.setProperty('animation-iteration-count', this._loop);
+        this._loop = null;
+      }
+      this.css.setProperty('animation-name', 'play');
+    }
+    this._running = !this._running;
   }
 
+  pauseAfter(): void {
+    this._loop = this.css.getPropertyValue('--loop');
+    this.css.setProperty('animation-iteration-count', '1');
+    this._running = false;
+    // this._sprite.addEventListener('animationend', () => {
+    //   this.css.setProperty('animation-iteration-count', this._loop);
+    //   console.log('animation ended.');
+    // })
+  }
 }
 
 customElements.define('sprite-player', SpritePlayer);
